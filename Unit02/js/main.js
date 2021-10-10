75339cafdba54cfbb9e03feaa8bbf6bf5d0fd935
millerh8 <henryjosephmiller@gmail.com>
2021-10-09 21:50:39 -0600

    Final js fix

--- Unit02/js/main.js
+++ Unit02/js/main.js
@@ -1,8 +1,9 @@
 document.querySelector('header > h1').innerText = "Inspector Clouseau";
 document.querySelector('header > h2').innerText = "You sir, are the idiom!";
 document.querySelector('#windowURL').innerText = `The page URL is ${document.URL}`;
+document.querySelector('#docTitle').innerText = `The document title is ${document.title}`;
+document.querySelector('#pageUpdate').innerText = `The page was last modified ${document.lastModified}`;
 
-function reDisplay() {
+
     document.querySelector('#myWindow').innerText = `Window size is ${window.innerWidth} pixels wide by ${window.innerHeight} pixels tall.`;
     document.querySelector('#windowOffset').innerText = `Window offset is ${window.screenX} pixels from the left edge and ${window.screenY} pixels from the top.`;
-}
