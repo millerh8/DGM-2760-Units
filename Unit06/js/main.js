9eb35f8c578c8d7f68b117cb30421edf2f3c8d3d
millerh8 <henryjosephmiller@gmail.com>
2021-11-23 17:45:18 -0700

    updated story

--- Unit06/js/main.js
+++ Unit06/js/main.js
@@ -1,5 +1,15 @@
 document.querySelector('#create-story').onclick = () => {
-const nouns = document.querySelector('#text1').value
-nounsArr = nouns.split(/\s+/); //.replace(/\s+/," ").split(" ")
-console.log(nounsArr)
+    const nouns = document.querySelector('#noun').value
+    nounsArr = nouns.split(/\s+/); //.replace(/\s+/," ").split(" ")
+    const adjectives = document.querySelector('#adjective').value
+    adjArr = adjectives.split(/\s+/);
+    const verbs = document.querySelector('#verb').value
+    verbsArr = verbs.split(/\s+/);
+
+    let story = `There once was a ${nounsArr[0]} that lived in a ${adjArr[0]} house with their friends: Rick, ${nounsArr[1]}, and James. 
+    One day, while they ${verbsArr[0]}ed in their yard, they discovered a rare ${nounsArr[2]}. It began to ${verbsArr[1]} 
+    ${adjArr[1]} colors and grew into a giant plant. They all wanted to ${verbsArr[2]} the plant, except for James, he was feeling too ${adjArr[2]}. 
+    To this day James has not seen his friends.`
+
+    document.querySelector('#story').innerText = story;
 }
